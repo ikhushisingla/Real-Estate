c1e6b73e443b33ccc8155d8ec619971dcb23039b
ikhushisingla <singlakhushi822@gmail.com>
2023-08-21 12:44:25 +0530

    hero section done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import "./App.css"
 
 const App = () => {
   return (
     <div className='App'>
-      <Header/>
+      <div>
+        <div className='white-gradient'/>
+          <Header/>
+          <Hero/>
+      </div>
     </div>
   )
 }
