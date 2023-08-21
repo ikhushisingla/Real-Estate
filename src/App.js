d493cff9c2a6aae13e560415be350e01da0d3f79
ikhushisingla <singlakhushi822@gmail.com>
2023-08-21 12:52:00 +0530

    companies section done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import "./App.css"
+import Companies from './components/Companies/Companies'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Header/>
           <Hero/>
       </div>
+      <Companies/>
     </div>
   )
 }
